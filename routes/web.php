bfa10e1837a5c535a2ba71a02f9d9690f41b4a62
hrafsa <hrafsa.a@gmail.com>
2025-08-12 09:11:53 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+
+use App\Models\Post;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
+
+Route::get('/', function () {
+    return view('home',
+        ['title' => 'Ini Halaman Home']
+    );
+});
+
+Route::get('/about', function () {
+    return view('about',
+        ['title' => 'Ini Halaman About']
+    );
+});
+
+Route::get('/posts', function () {
+    return view('posts',
+        ['title' => 'Ini Halaman Blog',
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact',
+        ['title' => 'Ini Halaman Contact']
+    );
+});
+
+Route::get('/post/{id}', function ($id) {
+
+    $post = Post::find($id);
+    return view('post', ['title' => 'Halaman Single Post', 'post' => $post]);
+});
+
+Route::get('/menu', [MenuController::class,'index']);
+
